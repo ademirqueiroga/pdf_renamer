5ba4f960a40dae9caf920a81997db958539901b0
Ademir Queiroga <ademirneto2007@gmail.com>
2018-04-14 22:59:54 -0300

    add gitignore

--- rename.py
+++ rename.py
@@ -17,7 +17,6 @@ def process(source, dest):
                 pdfWriter.addPage(page)
                 pdfWriter.write(newFile)
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Invalid arguments."
